a0c7c6a30293433f6cc787279cc6aaa66ed692bf
user <user@DESKTOP-8565OH6>
2025-11-06 14:03:16 +0900

    잘못된 인터페이스 수정 김설규 commit;

--- customerManager/src/cm/man/CustomerDetailDAO.java
+++ customerManager/src/cm/man/CustomerDetailDAO.java
@@ -1,5 +1,11 @@
 package cm.man;
 
-public class CustomerDetailDAO implements CustomerDetailDAOImpl {
+import java.util.List;
 
+public interface CustomerDetailDAO {
+	/**
+	고객 전체를 조회하는 리스트
+	@author	김설규
+	*/
+	public List<CustomerDetailDTO> CustomerDetailList();
 }
--- customerManager/src/cm/man/CustomerDetailDAOImpl.java
+++ customerManager/src/cm/man/CustomerDetailDAOImpl.java
@@ -1,5 +1,36 @@
 package cm.man;
 
-public interface CustomerDetailDAOImpl {
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
+import db.util.DBUtil;
+
+public class CustomerDetailDAOImpl implements CustomerDetailDAO{
+	/**
+	고객 전체를 조회하는 리스트
+	@author	김설규
+	@return	list
+	*/
+	@Override
+	public List<CustomerDetailDTO> CustomerDetailList() {
+		List<CustomerDetailDTO> list = new ArrayList<CustomerDetailDTO>();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql;
+		
+		try {
+			sql = "";
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			DBUtil.close(rs);
+			DBUtil.close(pstmt);
+		}
+		
+		return list;
+	}
+	
 }
