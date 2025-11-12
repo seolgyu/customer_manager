c5c58ebc6f9409c74a79d5e30b4575c4716d8701
user <user@DESKTOP-8565OH6>
2025-11-12 15:20:14 +0900

    상품 분류, 상품테이블 가데이터 txt 김설규

--- customerManager/src/cm/cmm/CustomerDetailUI.java
+++ customerManager/src/cm/cmm/CustomerDetailUI.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.List;
 
+
+
 public class CustomerDetailUI {
 	private CustomerDetailDAO dao = new CustomerDetailDAOImpl();
 	private BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
